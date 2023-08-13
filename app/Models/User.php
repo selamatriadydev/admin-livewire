d6d9d08c4092d8e935b391889552f93625c5c948
selamatriadydev <selamat.riady123@gmail.com>
2023-08-13 17:30:12 +0700

    clean spatie

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,12 +8,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use Spatie\Permission\Traits\HasRoles as spatieRole;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
-    use spatieRole;
 
     /**
      * The attributes that are mass assignable.
