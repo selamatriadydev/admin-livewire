15001e641957525c97305987687a3af73058ec11
selamatriadydev <selamat.riady123@gmail.com>
2023-08-23 00:09:28 +0700

    ubah layout users

--- resources/views/livewire/settings/users-management.blade.php
+++ resources/views/livewire/settings/users-management.blade.php
@@ -32,73 +32,82 @@
                 <!-- card  -->
                 <div class="card">
                     <!-- card header  -->
-                    <div class="card-header d-md-flex border-bottom-0">
-                        <div class="flex-grow-1">
-                            <h5 class="card-title h3">Manajemen Role</h5>
-                        </div>
-                        <div class="mt-3 mt-md-0">
-                            @if ($actCreate)
-                                <button type="button" wire:click="toggleOffcanvas" class="btn btn-primary ms-2" data-bs-toggle="tooltip" data-placement="top" title="Detail data">
-                                    New Data
-                                </button>
-                            @endif
-                            @if (count($selectedItems))
-                                <button type="button" wire:click="deleteSelectedItemsConfirm" class="btn btn-danger ms-2" data-bs-toggle="tooltip" data-placement="top" title="Detail data">
-                                    Delete Selected {{ count($selectedItems) }} Data
-                                </button>
-                            @endif
+                    <div class="card-header">
+                        <div class="row">
+                            <div class=" col-lg-3 col-md-6">
+                                <input type="search" class="form-control " placeholder="Search Files...">
+                            </div>
+                            <div class="col-lg-4 col-md-6 d-flex align-items-center mt-3 mt-md-0">
+                                <label class="form-label me-2 mb-0">Status</label>
+                                <select class="form-select" aria-label="Default select example">
+                                    <option selected="">Online</option>
+                                    <option value="1">Offline</option>
+                                    <option value="2">Online</option>
+                                </select>
+                            </div>
+                            <div class="col-lg-5 text-lg-end mt-3 mt-lg-0">
+                                @if ($actCreate)
+                                    <a href="#!" wire:click="toggleOffcanvas" class="btn btn-primary me-2" data-bs-toggle="tooltip" data-placement="top" title="New data">+ Add New Data</a>
+                                @endif
+                                <a href="#!" class="btn btn-light ">Export</a>
+                                @if (count($selectedItems))
+                                    <a href="#!" class="btn btn-danger " wire:click="deleteSelectedItemsConfirm" data-bs-toggle="tooltip" data-placement="top" title="Delete Selected data">Delete Selected {{ count($selectedItems) }} Data</a>
+                                @endif
+                            </div>
                         </div>
                     </div>
                     <!-- table  -->
-                    <div class="table-responsive">
-                        <table class="table text-nowrap mb-0">
-                            <thead class="table-light">
-                                <tr>
-                                    <th scope="col" width="6%">Check</th>
-                                    <th scope="col" width="5%">#</th>
-                                    @foreach ($tableHead as $head)
-                                        <th scope="col">{{ $head }}</th>
-                                    @endforeach
-                                    <th scope="col" width="20%">Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @forelse ($users as $item)
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table text-nowrap mb-0">
+                                <thead class="table-light">
                                     <tr>
-                                        <td>
-                                            <div class="form-check">
-                                                <input class="form-check-input" type="checkbox" wire:model="selectedItems" value="{{ $item->id }}">
-                                            </div>
-                                        </td>
-                                        <th scope="row">{{ $loop->iteration }}</th>
-                                            @foreach ($tableBody as $tdata)
-                                                <td>{{ $item->$tdata }}</td>
-                                            @endforeach
+                                        <th scope="col" width="6%">Check</th>
+                                        <th scope="col" width="5%">#</th>
+                                        @foreach ($tableHead as $head)
+                                            <th scope="col">{{ $head }}</th>
+                                        @endforeach
+                                        <th scope="col" width="20%">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @forelse ($users as $item)
+                                        <tr>
                                             <td>
-                                                @if ($actDetail)
-                                                    <button type="button" class="btn btn-info btn-sm" data-bs-toggle="tooltip" data-placement="top" title="Detail data">
-                                                        Detail
-                                                    </button>
-                                                @endif
-                                                @if ($actUpdate)
-                                                    <button type="button" wire:click="edit('{{ $item->id }}')" class="btn btn-warning btn-sm" data-bs-toggle="tooltip" data-placement="top" title="Edit data">
-                                                        Edit
-                                                    </button>
-                                                @endif
-                                                @if ($actDelete)
-                                                    <button type="button" wire:click="deleteConfirm('{{ $item->id }}')" class="btn btn-danger btn-sm" data-bs-toggle="tooltip" data-placement="top" title="Delete data">
-                                                            Delete
-                                                        </button>
-                                                @endif
+                                                <div class="form-check">
+                                                    <input class="form-check-input" type="checkbox" wire:model="selectedItems" value="{{ $item->id }}">
+                                                </div>
                                             </td>
-                                    </tr>
-                                @empty
-                                    <tr>
-                                        <td colspan="4" class="text-center">Data tidak tersedia</td>
-                                    </tr>
-                                @endforelse
-                            </tbody>
-                        </table>
+                                            <th scope="row">{{ $loop->iteration }}</th>
+                                                @foreach ($tableBody as $tdata)
+                                                    <td>{{ $item->$tdata }}</td>
+                                                @endforeach
+                                                <td>
+                                                    @if ($actDetail)
+                                                        <button type="button" class="btn btn-info btn-sm" data-bs-toggle="tooltip" data-placement="top" title="Detail data">
+                                                            Detail
+                                                        </button>
+                                                    @endif
+                                                    @if ($actUpdate)
+                                                        <button type="button" wire:click="edit('{{ $item->id }}')" class="btn btn-warning btn-sm" data-bs-toggle="tooltip" data-placement="top" title="Edit data">
+                                                            Edit
+                                                        </button>
+                                                    @endif
+                                                    @if ($actDelete)
+                                                        <button type="button" wire:click="deleteConfirm('{{ $item->id }}')" class="btn btn-danger btn-sm" data-bs-toggle="tooltip" data-placement="top" title="Delete data">
+                                                                Delete
+                                                            </button>
+                                                    @endif
+                                                </td>
+                                        </tr>
+                                    @empty
+                                        <tr>
+                                            <td colspan="4" class="text-center">Data tidak tersedia</td>
+                                        </tr>
+                                    @endforelse
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                     <!-- card footer  -->
                     <div class="card-footer bg-white text-center">
